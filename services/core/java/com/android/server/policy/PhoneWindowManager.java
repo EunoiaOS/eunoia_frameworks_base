77958be204ff5840256c4fc83e80622d0dd436c6
ghbhaha <ghbhaha@gmail.com>
2015-08-09 01:23:33 +0800

    base: Add three-fingers-swipe to screenshot [2/3]
    The feature is ported from Oppo ColorOS.
    With the options on, users can swipe down
    on the screen to have a screenshot.
    
    original smali port by: wuxianlin
    
    BenzoEdit: Fixed import for Pie
    AicpEdit : Fixed arguments for 10 eyosen
    	   Ported to 11 by eyosen
    
    @neobuddy89: Clean up initialization
    
    Change-Id: I53c291ae768ae3b02b39ac48424fb8cc810f2101

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -665,6 +665,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private int mForceNavbar = -1;
 
+    private OPGesturesListener mOPGestures;
+    private boolean haveEnableGesture = false;
+
     // Tracks user-customisable behavior for certain key events
     private Action mBackLongPressAction;
     private Action mHomeLongPressAction;
@@ -1080,6 +1083,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
                     LineageSettings.System.VOLUME_ANSWER_CALL), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(LineageSettings.System.getUriFor(
+                    LineageSettings.System.THREE_FINGER_GESTURE), false, this,
                     UserHandle.USER_ALL);
 
             updateSettings();
@@ -3349,6 +3355,19 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (mLineageHardware.isSupported(LineageHardwareManager.FEATURE_KEY_DISABLE)) {
                     mLineageHardware.set(LineageHardwareManager.FEATURE_KEY_DISABLE,
                             mForceNavbar == 1);
+                }
+            }
+
+            boolean threeFingerGesture = LineageSettings.System.getIntForUser(resolver,
+                    LineageSettings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT) == 1;
+            if (mOPGestures != null) {
+                if (haveEnableGesture != threeFingerGesture) {
+                    haveEnableGesture = threeFingerGesture;
+                    if (haveEnableGesture) {
+                        mWindowManagerFuncs.registerPointerEventListener(mOPGestures, DEFAULT_DISPLAY);
+                    } else {
+                        mWindowManagerFuncs.unregisterPointerEventListener(mOPGestures, DEFAULT_DISPLAY);
+                    }
                 }
             }
 
@@ -6725,6 +6744,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         mLineageHardware = LineageHardwareManager.getInstance(mContext);
+        mOPGestures = new OPGesturesListener(mContext, new OPGesturesListener.Callbacks() {
+            @Override
+            public void onSwipeThreeFinger() {
+                mHandler.removeCallbacks(mScreenshotRunnable);
+                mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
+                mScreenshotRunnable.setScreenshotSource(SCREENSHOT_KEY_OTHER);
+                mHandler.post(mScreenshotRunnable);
+            }
+        });
         // Ensure observe happens in systemReady() since we need
         // LineageHardwareService to be up and running
         mSettingsObserver.observe();
