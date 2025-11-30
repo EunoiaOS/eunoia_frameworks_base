387161d0b4238e2fed689b76752e87545e22180e
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2025-12-01 05:37:45 +0700

    SystemUI: Preserve unicode bolt ⚡ color
    Co-authored-by: Trishiraj <gamerprince.exp@gmail.com>
    Change-Id: I6162b5b245f9535d961eb6b77ffad843bcddd14f
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -462,7 +462,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         if (mBatteryPercentView != null) {
             mEstimateText = null;
             String percentText = NumberFormat.getPercentInstance().format(mLevel / 100f);
-            String bolt = "\u26A1\uFE0E";
+            String bolt = "\u26A1";
             percentText = isCharging() ? bolt + " " + percentText : percentText;
             // Setting text actually triggers a layout pass (because the text view is set to
             // wrap_content width and TextView always relayouts for this). Avoid needless
